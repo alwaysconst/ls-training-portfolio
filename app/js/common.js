809e4e35a88774c9e2de4dad16f897f822b8eca6
unknown <technik@Technik.habitat.gb1>
2015-06-18 09:39:24 +1100

    add_modal_

---
+++ app/js/common.js
@@ -0,0 +1,6 @@
+$ (function() {
+	if (!Modernizr.input.placeholder){
+		$('input, textarea').placeholder();
+	}
+
+});
---
+++ app/js/input-file.js
@@ -0,0 +1,9 @@
+(function($){
+
+/* Обновляем label текст */
+$('.input-file__input').on('change', function() {
+    var $input = $(this);
+    $input.siblings('.input-file__text').text($input.val().replace(/([^\\]*\\)*/,'') )
+});
+
+}(jQuery));
